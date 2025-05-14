5ff1c5f89f5d44d709bf98400e1b859f0dad5ddc
prodev1996 <bhandarirajiv25@gmail.com>
2025-05-14 13:59:28 +0930

    Updated homepage content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,26 +8,26 @@ const Navbar = () => {
   return (
     <>
       {/* Top Contact Bar */}
-      <div className="top-contact-bar" data-aos="fade-down" data-aos-delay="100">
+      <div className="top-contact-bar">
         <span>Email: info@primeroofcare.com</span>
         <span> | </span>
         <span>Call: 08 8004 6134</span>
       </div>
 
       {/* Main Navbar */}
-      <header className="navbar" data-aos="fade-down" data-aos-delay="300">
+      <header className="navbar">
         <div className="navbar-left">
           <img src={logo} alt="Prime Roof Care" className="navbar-logo" />
           <h1 className="company-name">Prime Roof Care</h1>
         </div>
 
         <nav className="navbar-links">
-          <Link to="home" smooth={true} duration={500}>Home</Link>
-          <Link to="about" smooth={true} duration={500}>About</Link>
-          <Link to="services" smooth={true} duration={500}>Services</Link>
-          <Link to="gallery" smooth={true} duration={500}>Gallery</Link>
-          <Link to="testimonials" smooth={true} duration={500}>Testimonials</Link>
-          <Link to="contact" smooth={true} duration={500}>Contact</Link>
+          <Link to="home" smooth={true} duration={500} offset={-90}>Home</Link>
+          <Link to="about" smooth={true} duration={500} offset={-90}>About</Link>
+          <Link to="services" smooth={true} duration={500} offset={-90}>Services</Link>
+          <Link to="gallery" smooth={true} duration={500} offset={-90}>Gallery</Link>
+          <Link to="testimonials" smooth={true} duration={500} offset={-90}>Testimonials</Link>
+          <Link to="contact" smooth={true} duration={500} offset={-90}>Contact</Link>
           <a href="#quote" className="quote-button">Get a Free Quote</a>
         </nav>
       </header>
